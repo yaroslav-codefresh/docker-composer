2a2ccbb87b0c7aec3a3ff7c6eb67abf42cbdbc54
yaroslav-codefresh <45195052+yaroslav-codefresh@users.noreply.github.com>
2018-12-10 16:14:58 +0200

    Update testable.js

--- testable.js
+++ testable.js
@@ -4,12 +4,3 @@
 // };
 //
 // aaa.say = aaa.say.bind({name: "bbb"});
-//
-// aaa.say();
-//
-// Promise.resolve().then(() => console.log("rrr"))
-//
-//
-// (function () {
-//   console.log('xxx');
-// })();
